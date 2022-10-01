4440d0f04d63fbad7f6f7ee512244dc29243188b
Sachin/ <87219101+RookieProgrammerSachin@users.noreply.github.com>
2022-10-01 08:10:42 +0530

    Add files via upload
    made responsive, added AOS

--- app.js
+++ app.js
@@ -1,33 +1,42 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    var time = (new Date().getTime() / 1000) + (86400*19) + 1
-
-    var flipdown = new FlipDown(time).start().ifEnded(()=>{
-        console.log("mudinchu")
-    })
+document.querySelector('.hamburger').addEventListener('click', (e)=>{
+  document.querySelector('.nav-buttons-mobile').classList.toggle('hidden-nav')
+  let i=0;
+  document.querySelectorAll('.hamburger span').forEach((span)=>{ 
+    i+=1; 
+    span.classList.toggle(`close-${i}`);
+   })
 })
 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.querySelector(".header-nav img").style.width = "60px";
-    document.querySelector(".header-nav .brand-name").style.fontSize = "1.2rem";
-    document.querySelectorAll(".header-nav .nav-buttons .nav-btn").forEach((btn)=>{btn.style.fontSize = "0.95rem"});
-    
+    document.querySelector(".header-nav img").style.width = "80px";
+    document.querySelector(".header-nav .brand-name").style.fontSize = "1.3rem";
+    document.querySelectorAll(".header-nav .nav-buttons .nav-btn").forEach((btn)=>{btn.style.fontSize = "1.2rem"});
+    document.querySelector(".hamburger").style.top = "26px";
     // document.getElementById("logo").style.fontSize = "25px";
   } else {
     document.querySelector(".header-nav img").style.width = "100px";
     document.querySelector(".header-nav .brand-name").style.fontSize = "";
     document.querySelectorAll(".header-nav .nav-buttons .nav-btn").forEach((btn)=>{btn.style.fontSize = ""});
+    document.querySelector(".hamburger").style.top = "";
     // document.getElementById("navbar").style.padding = "80px 10px";
     // document.getElementById("logo").style.fontSize = "35px";
   }
 } 
 
+AOS.init();
+
+document.addEventListener("DOMContentLoaded",()=>{
+  var time = new Date().getTime()/1000 + (86400*18) + 1;
+
+  var flipdown = new FlipDown(time).start().ifEnded(()=>{
+      console.log("mudinchu")
+  })
+})
+
 particlesJS.load("particles-js", "./static/particlesjs-config.json", function() {
     console.log('callback - particles-js config loaded');
   });
 
-document.querySelector('.hamburger').addEventListener('click', (e)=>{
-    document.querySelector('.nav-buttons-mobile').classList.toggle('hidden-nav')
-})
